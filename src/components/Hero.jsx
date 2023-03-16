313c9d8fd94a1dfe0fbe27d120beb8c185d92c55
Ansh kumain <104789216+vector17002@users.noreply.github.com>
2023-03-16 13:30:07 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import {motion} from 'framer-motion'
 import {styles} from '../styles'
 import {ComputersCanvas} from './canvas'
-
+import {isMobile} from 'react-device-detect'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
+    <section className='relative w-full h-screen mx-auto '>
          <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
             <div className='flex flex-col justify-center items-center mt-5 '>
              <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
@@ -17,8 +17,8 @@ const Hero = () => {
               <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am a full stack web developer<br className='sm:block hidden'/> with expertise in Front End</p>
              </div>
          </div>
-         <ComputersCanvas/>
-         <div className='absolute xs:bottom-10 bottom-32 flex justify-center items-center w-full'>
+         {isMobile ?  <div className='hidden'></div>: <ComputersCanvas/> }
+        <div className='absolute xs:bottom-10 bottom-32 flex justify-center items-center w-full'>
              <a href='#about'><div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
               <motion.div 
                 animate={{
